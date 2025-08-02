768a8b4d861c528021ac2c23e177ee7169967fe3
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-02 15:19:25 +0600

    third task completed

---
+++ sum-of-odd-numbers.js
@@ -0,0 +1,15 @@
+let sum = 0;
+for(let i = 91; i <= 129; i++){
+    if(i %2 != 0){
+        sum += i;
+    }
+}
+console.log(sum);
+
+let sum2 = 0;
+for(let j = 51; j <= 85; j++){
+    if(j %2 === 0){
+        sum2 += j;
+    }
+}
+console.log(sum2);
