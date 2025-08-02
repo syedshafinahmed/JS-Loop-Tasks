5e22500e19e304fe8667adfac22d18d70dbe0115
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-02 20:28:45 +0600

    3 done

---
+++ array/3.js
@@ -0,0 +1,7 @@
+let books = ["Python Basics", "JavaScript Essentials", "HTML & CSS Guide", "Data Structures"];
+if(books.includes("JavaScript Essentials")){
+    console.log("It's here!!");
+}
+else{
+    console.log("No!! It isn't here.");
+}
