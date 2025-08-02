3be1c211a33608552aa8bc7cd9056525138ac796
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-02 20:24:15 +0600

    2 done

---
+++ array/2.js
@@ -0,0 +1,8 @@
+let array = ["Cox's Bazar", "Sylhet", "Bandorban"];
+console.log(array);
+array.push("Nilgiri");
+console.log(array);
+array.push("Himchori", "Kuakata");
+console.log(array);
+array.pop();
+console.log(array);
