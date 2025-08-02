de54a10fe0a91c9225d8d566deb0e850c39db1d4
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-02 16:01:58 +0600

    completed continue tasks

---
+++ continue/skip-five.js
@@ -0,0 +1,9 @@
+for(i = 55; i <= 85; i++){
+    if(i%2 === 0){
+        continue;
+    }
+    if(i%5 === 0){
+        continue;
+    }
+    console.log(i);
+}
---
+++ continue/skip-odd.js
@@ -0,0 +1,8 @@
+for(let i = 1; i <= 40; i++){
+    if(i %2 != 0){
+        continue;
+    }
+    else{
+        console.log(i);
+    }
+}
