11cc3781ba0f3cc4dd80c8d0697c983006f2fb31
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-02 20:49:40 +0600

    5

---
+++ array/5.js
@@ -0,0 +1,8 @@
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6, 7, 8, 9, 10];
+array3 = array1.concat(array2);
+array1len = array1.length;
+array2len = array2.length;
+console.log(array3);
+console.log(array1len);
+console.log(array2len);
